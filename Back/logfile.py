aacf622d43a16f01c46749498888c5aae6f90201
Ralph <ralph.souza@olist.com>
2021-01-06 14:27:36 +0000

    Refatoração de código na função list_mktplaces de logfiles.py para marketplace.py.

--- Back/logfile.py
+++ Back/logfile.py
@@ -5,15 +5,3 @@ from archive import *
 def save_log(message) -> None:
     message = f"[{datetime.today().strftime('%d/%m/%Y %H:%M:%S')}]: {message}\n"
     write("../Log/log.txt", message)
-
-
-def list_marketplaces() -> list:
-    file_lines = []
-    file = open(save_log, 'a')
-    for line in file:
-        clear_line = line.strip()
-        per_line = clear_line.split(';')
-        formated_line = f"{per_line[0], per_line[1]}"
-        file_lines.append(formated_line)
-    file.close()
-    return file_lines
---
+++ Back/marketplace.py
@@ -0,0 +1,11 @@
+def list_marketplaces(path) -> list:
+    file_lines = []
+    file = open(path, 'r')
+    for line in file.readlines():
+        clear_line = line.strip().split(';')
+        formated_line = {"name": clear_line[0], "description": clear_line[1]}
+        file_lines.append(formated_line)
+    file.close()
+    return file_lines
+
+print(list_marketplaces())
