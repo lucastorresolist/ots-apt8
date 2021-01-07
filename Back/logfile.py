2a1e7243a2bfd6896b7e151f782512d760451f22
Ralph <ralph.souza@olist.com>
2021-01-07 15:00:57 +0000

    Refatoração de código e criação da função read_log.

--- Back/logfile.py
+++ Back/logfile.py
@@ -1,7 +1,21 @@
 from datetime import datetime
 from Back.archive import *
 
+import json
 
-def save_log(message) -> None:
-    message = f"[{datetime.today().strftime('%d/%m/%Y %H:%M:%S')}]: {message}\n"
+import sys
+sys.path.append('.')
+
+
+def save_log(message, type) -> None:
+    date = datetime.today().strftime('%d/%m/%Y %H:%M:%S')
+    message =  "{" + f'"date": "{date}", "message": "{message}", "type": "{type}"' + "}"
     write("../Log/log.txt", message)
+
+def read_log():
+    file = open('../Log/log.txt', 'r').readlines()
+    lines = []
+    for line in file:
+        lines.append(json.loads(line))
+    return lines
+
