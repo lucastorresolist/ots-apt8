e0973f2f21d9b1ff0d6b88742dbea2348697f525
Ralph <ralph.souza@olist.com>
2021-01-07 09:57:02 +0000

    Criação da função de list_category em category.py.

--- Back/category.py
+++ Back/category.py
@@ -7,11 +7,19 @@ file_route = '../Data/category.txt'
 
 
 def create_category(name: str, description: str) -> None:
-    file.open(file_route, 'a')
+    file = open(file_route, 'a')
     file.write(f'{name};{description}\n')
     file.close()
-    action = f'Category {name} successfully created!'
-    write(action)
+    line = f'Category {name} successfully created!'
+    write(line)
 
 
-
+def list_category(path) -> list:
+    file_lines = []
+    file = open(path, 'r')
+    for line in file.readlines():
+        clear_line = line.strip().split(';')
+        formated_line = {'name': clear_line[0], 'descriprion': clear_line[1]}
+        file_lines.append(formated_line)
+    file.close()
+    return file_lines
