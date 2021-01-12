da04b7d08cf83984acf05b809408b40c16663af8
marcosdcl <marcosdouglasclima@gmail.com>
2021-01-12 16:03:43 -0300

    Updated on dao_db to handle models

--- Back/dao_db/dao_marketplaces.py
+++ Back/dao_db/dao_marketplaces.py
@@ -1,24 +1,31 @@
 import psycopg2
+from Back.models.marketplace import Marketplace
+
+
 _host = 'pgsql08-farm15.uni5.net'
 _user = 'topskills13'
 _password = 'olist123'
 _database = 'topskills13'
 connection_string = f"host={_host} user={_user} dbname={_database} password={_password}"
 
-def save_mkplace(name:str, description:str) -> None:
+
+def save_mkplace(marketplace:Marketplace) -> None:
     conn = psycopg2.connect(connection_string)
     cursor = conn.cursor()
-    sql = f"INSERT INTO marketplaces (name, description) values ('{name}','{description}')"
+    sql = f"INSERT INTO marketplaces (name, description) values ('{marketplace.name}','{marketplace.description}')"
     cursor.execute(sql)
     conn.commit()
     cursor.close()
     conn.close()
 
-
 def list_mkplaces():
     conn = psycopg2.connect(connection_string)
     cursor = conn.cursor()
-    sql = "select name, description from marketplaces"
+    sql = "select name, description, id from marketplaces"
     cursor.execute(sql)
-    list_mkplaces = cursor.fetchall()
-    return list_mkplaces
+    result = cursor.fetchall()
+    marketplaces = []
+    for item in result:
+        marketplace = Marketplace(item[0], item[1], item[2])
+        marketplaces.append(marketplace)
+    return marketplaces
