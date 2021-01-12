ec4c2a3e1419382ebdcc8016ba4b7be8f0e5b1bb
marcosdcl <marcosdouglasclima@gmail.com>
2021-01-12 16:02:53 -0300

    Updated on controllers to handle models

--- Back/controller/controller_sellers.py
+++ Back/controller/controller_sellers.py
@@ -1,7 +1,9 @@
+from Back.models.seller import Seller
 from Back.dao_db.dao_sellers import save_seller, list_sellers
 
-def save_sell(name: str, phone: str, email: str) -> None:
-    save_seller(name, phone, email)
+
+def save_sell(seller:Seller) -> None:
+    save_seller(seller)
 
 def list_sell() -> list:
     list_sel = list_sellers()
