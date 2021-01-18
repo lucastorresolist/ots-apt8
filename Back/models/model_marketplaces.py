50ffc334b0c46b0a7226d7202add6a24202d5732
marcosdcl <marcosdouglasclima@gmail.com>
2021-01-18 13:32:11 -0300

    Added properties and setters to model marketplace

--- Back/models/model_marketplaces.py
+++ Back/models/model_marketplaces.py
@@ -1,6 +1,30 @@
 class Marketplace:
 
-    def __init__(self, name:str, description:str, id:int = None) -> None:
-        self.id = id
-        self.name = name
-        self.description = description
+    def __init__(self, name:str, description:str, id:int = None)-> None:
+        self.__id = id
+        self.__name = name
+        self.__description = description
+
+    @property
+    def id(self)-> int:
+        return self.__id
+
+    @property
+    def name(self)-> str:
+        return self.__name
+
+    @property
+    def description(self)-> str:
+        return self.__description
+
+    @id.setter
+    def id(self, id)-> None:
+        self.__id = id
+
+    @name.setter
+    def name(self, name)-> None:
+        self.__name = name
+
+    @description.setter
+    def description(self, description)-> None:
+        self.__description = description
