eca0b36bd4bbdf96ee76f6ab06ac5ab4d1f51d7d
Matheus B. Lopes <matheus.bachiste@olist.com>
2021-01-20 11:18:35 -0300

    Removed properties

--- Back/models/sellers_model.py
+++ Back/models/sellers_model.py
@@ -1,50 +1,16 @@
-from sqlalchemy.ext.declarative import declarative_base
+from Back.models.base_model import BaseModel
 from sqlalchemy import Column, Integer, String
 
-Base = declarative_base()
 
-class Seller:
-    __tablename__ = 'seller'
+class Seller(BaseModel):
+    __tablename__ = 'sellers'
 
-    id = Column( Integer, primary_key=True )
-    name = Column( String(length=100) )
-    telephone = Column( String(length=20) )
+    name = Column( String(length=50) )
+    phone = Column( String(length=50) )
     email = Column( String(length=50) )
 
-    def __init__(self, name:str, telephone:str, email: str, id: str = None) -> None:
-        self.__name = name
-        self.__telephone = telephone
-        self.__email = email
-        self.__id = id
-
-    @property
-    def id(self)-> int:
-        return self.__id
-
-    @property
-    def name(self)-> str:
-        return self.__name
-
-    @property
-    def telephone(self)-> str:
-        return self.__telephone
-
-    @property
-    def email(self)-> str:
-        return self.__email
-
-    @id.setter
-    def id(self, id)-> None:
-        self.__id = id
-
-    @name.setter
-    def name(self, name)-> None:
-        self.__name = name
-
-    @telephone.setter
-    def telephone(self, phone)-> None:
-        self.__telephone = phone
-
-    @email.setter
-    def email(self, email)-> None:
-        self.__email = email
+    def __init__(self, name:str, phone:str, email: str, id: str = None) -> None:
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.id = id
