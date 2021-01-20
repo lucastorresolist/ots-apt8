1739a1bfc07149740b0e89423cbe0d90964721c0
Ralph <ralph.souza@olist.com>
2021-01-20 09:37:27 +0000

    Refactored files.

--- Back/controller/controller_marketplaces.py
+++
@@ -1,8 +0,0 @@
-from Back.controller.base_controller import BaseController
-from Back.dao_db.dao_marketplaces import MarketplaceDao
-
-
-class MarketplaceController(BaseController):
-    def __init__(self)-> None:
-        self.__dao = MarketplaceDao()
-        super().__init__(self.__dao)
