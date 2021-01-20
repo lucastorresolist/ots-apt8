eeae9c8ed920e5605e8919bb35794063b6ee61ea
Matheus B. Lopes <matheus.bachiste@olist.com>
2021-01-20 11:20:18 -0300

    Updated type_model atribute

--- Back/dao_db/base_dao.py
+++ Back/dao_db/base_dao.py
@@ -4,21 +4,26 @@ from ..models.base_model import BaseModel
 
 class BaseDao:
     def __init__(self, type_model) -> None:
-        self.__type_model = type_model
+        self.type_model = type_model
 
     def save(self, model: BaseModel) -> None:
         with Session() as session:
             session.add(model)
             session.commit()
 
+    def update(self, model: BaseModel) -> None:
+        with Session() as session:
+            session.add(model)
+            session.commit()
+
     def read_all(self) -> list:
         with Session() as session:
-            result = session.query(self.__type_model).all()
+            result = session.query(self.type_model).all()
         return result
 
     def read_by_id(self, id: int) -> BaseModel:
         with Session() as session:
-            result = session.query(self.__type_model).filter_by(id=id).first()
+            result = session.query(self.type_model).filter_by(id=id).first()
         return result    
     
     def delete(self, model: BaseModel) -> None:
