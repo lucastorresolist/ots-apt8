c361e8dc35811a749e3c36472d7f44a4baddb1ff
Ralph <ralph.souza@olist.com>
2021-01-20 09:46:22 +0000

    Code refactoring.

--- Back/dao_db/marketplaces_dao.py
+++ Back/dao_db/marketplaces_dao.py
@@ -1,41 +1,7 @@
 from .base_dao import BaseDao
-from Back.models.model_marketplaces import Marketplace
+from Back.models.marketplaces_model import Marketplace
 
 
 class MarketplaceDao(BaseDao):
-    def create(self, model:Marketplace)-> None:
-        query = f"""INSERT INTO marketplaces 
-                    (name, description) 
-                    VALUES 
-                    ('{model.name}','{model.description}');"""
-        super().execute(query)
-
-    def read_by_id(self, id:int)-> Marketplace:
-        query = f"""SELECT name, description, id FROM marketplaces WHERE id={id};"""
-        result = super().read(query)[0]
-        marketplace = Marketplace(result[0], result[1], result[2])
-        return marketplace
-
-    def read_all(self)-> list:
-        marketplaces = []
-        query = f"""SELECT name, description, id FROM marketplaces;"""
-        list_result = super().read(query)
-        for result in list_result:
-            marketplace = Marketplace(result[0], result[1], result[2])
-            marketplaces.append(marketplace)
-        return marketplaces
-        
-    def delete(self, id:int)-> None:
-        query = f"""DELETE FROM marketplaces WHERE id={id};"""
-        super().execute(query)
-    
-    def update(self, model:Marketplace)-> None:
-        query = f"""UPDATE marketplaces 
-                    SET
-                        name = '{model.name}', 
-                        description = '{model.description}'
-                    WHERE id = {model.id};"""
-        super().execute(query)
-
-    def entity(self):
-        return 'Marketplaces'
+    def __init__(self):
+        super().__init__(Marketplace)
