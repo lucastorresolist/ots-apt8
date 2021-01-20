0118826e047166831500379649992c8bed810cbd
ThiagoFiorelli <36682785+ThiagoFiorelli@users.noreply.github.com>
2021-01-20 11:27:03 -0300

    Alterado controllers, daos, web de categorias para receber as bases.

--- Back/controller/base_controller.py
+++ Back/controller/base_controller.py
@@ -9,7 +9,7 @@ class BaseController:
 
     def create(self, model:object)-> None:
         request = self.__dao.save(model)
-        log = Log("Saved", self.__dao.entity())
+        log = Log("Saved", self.__dao.type_model)
         self.__log_controller.save(log)
         return request
 
@@ -18,17 +18,17 @@ class BaseController:
 
     def read_all(self)-> list:
         request = self.__dao.read_all()
-        log = Log("Listed", self.__dao.entity())
+        log = Log("Listed", self.__dao.type_model)
         self.__log_controller.save(log)
         return request
 
     def delete(self, id:int)-> None:
         model = self.read_by_id(id)
         self.__dao.delete(model)
-        log = Log("Delete", self.__dao.entity())
+        log = Log("Delete", self.__dao.type_model)
         self.__log_controller.save(log)
 
     def update(self, model:object)-> None:
         self.__dao.save(model)
-        log = Log("Update", self.__dao.entity())
+        log = Log("Update", self.__dao.type_model)
         self.__log_controller.save(log)
